7f9eca59b146cc29bc0f3b82ff9ab021ea1e8f76
Oluseyi Kusoro <ramzythecoder@gmail.com>
2025-01-18 15:09:45 +0100

    oya

---
+++ src/test/java/com/iclout/config/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.iclout.config.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@SpringBootTest
+@WebAppConfiguration
+public class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
